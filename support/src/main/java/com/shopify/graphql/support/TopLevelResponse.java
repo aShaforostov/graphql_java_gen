f8462b6e0b953fceb49936003cb9d9e840f54993
aShaforostov <Alexey_shaforostov@devit-team.com>
2017-12-15 14:34:35 +0200

    Update TopLevelResponse.java

--- support/src/main/java/com/shopify/graphql/support/TopLevelResponse.java
+++ support/src/main/java/com/shopify/graphql/support/TopLevelResponse.java
@@ -2,9 +2,12 @@ package com.shopify.graphql.support;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by eapache on 2015-11-17.
@@ -18,6 +21,22 @@ public class TopLevelResponse {
     public TopLevelResponse(JsonObject fields) throws InvalidGraphQLException {
         JsonElement errorsElement = fields.get(ERRORS_KEY);
         JsonElement dataElement = fields.get(DATA_KEY);
+        JsonObject dataCustom = fields.getAsJsonObject(DATA_KEY);
+
+        JsonParser parser = new JsonParser();
+        JsonElement element = parser.parse(fields.get(DATA_KEY).toString());
+        JsonObject obj = element.getAsJsonObject();
+        Set<Map.Entry<String, JsonElement>> entries = obj.entrySet();
+        
+        for (Map.Entry<String, JsonElement> entry: entries) {
+            try {
+                errorsElement = entry.getValue().getAsJsonObject().getAsJsonArray("userErrors");
+            }
+            catch (IllegalStateException e){
+                continue;
+            }
+
+        }
         if (dataElement != null && dataElement.isJsonNull()) {
             dataElement = null;
         }
